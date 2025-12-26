f045de47dca430e634b590268b045d7b3c02cfc5
LiuyiLi2000 <106932722+LiuyiLi2000@users.noreply.github.com>
2025-12-26 14:00:54 -0800

    Fix SPA fallback path restore and router basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+<BrowserRouter basename={import.meta.env.BASE_URL}>
+  <App />
+</BrowserRouter>
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
